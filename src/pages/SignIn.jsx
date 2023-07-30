66467505c6d141513a249de31ba7aedd6ba2aeaa
cychann <skcbdlhc129@naver.com>
2023-07-30 15:12:18 +0900

    feat: 회원가입 페이지 링크 추가

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,47 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { useNavigate } from 'react-router-dom';
 import { SignInForm } from '../components/Sign';
+import COLOR from '../constants/color';
+
+const SignInLayout = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const SignupContainer = styled.span`
+  margin-top: 1rem;
+`;
+
+const SignupText = styled.span`
+  margin-right: 0.8rem;
+`;
+
+const SignupBtn = styled.span`
+  text-decoration: underline;
+  color: ${COLOR.primary.blue};
+  font-weight: bold;
+  cursor: pointer;
+`;
 
 const SignIn = () => {
-  return <SignInForm />;
+  const navigate = useNavigate();
+  const goToSignUp = () => {
+    navigate('/signup');
+  };
+  return (
+    <SignInLayout>
+      <SignInForm />
+      <SignupContainer>
+        <SignupText>아직 계정을 만들지 않았다면?</SignupText>
+        <SignupBtn onClick={goToSignUp}>회원가입</SignupBtn>
+      </SignupContainer>
+    </SignInLayout>
+  );
 };
 
 export default SignIn;
