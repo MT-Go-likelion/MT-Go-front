e1acffb8335c4ddb632b96eab6a6504f2a484e1a
cychann <skcbdlhc129@naver.com>
2023-07-30 17:55:52 +0900

    fix: terms modal 배경 스크롤 막기

--- src/components/Modal/TermsModal.jsx
+++ src/components/Modal/TermsModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import COLOR from '../../constants/color';
 
@@ -61,6 +61,19 @@ const TermsModal = ({ setTermsModalOpen }) => {
   const closeModal = () => {
     setTermsModalOpen(false);
   };
+
+  useEffect(() => {
+    document.body.style.cssText = `
+      position: fixed; 
+      top: -${window.scrollY}px;
+      overflow-y: scroll;
+      width: 100%;`;
+    return () => {
+      const scrollY = document.body.style.top;
+      document.body.style.cssText = '';
+      window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
+    };
+  }, []);
 
   return (
     <ModalBackdrop onClick={closeModal}>
