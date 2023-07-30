72b92314abbb19f462667b02ba4f2be29b17dfd1
cychann <skcbdlhc129@naver.com>
2023-07-30 22:20:28 +0900

    feat: Navbar UI 구성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Outlet } from 'react-router-dom';
-import Footer from './components/Footer';
-import Header from './components/Header';
+import Footer from './components/Footer/Footer';
 import GlobalStyles from './styles/GlobalStyle';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Header />
+      <Navbar />
       <Outlet />
       <Footer />
     </>
