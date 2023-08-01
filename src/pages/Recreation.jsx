5a003963931e91cacdad2ae403b27a472a824ed2
cychann <skcbdlhc129@naver.com>
2023-08-01 13:13:39 +0900

    feat: 레크레이션 목록 grid UI

--- src/pages/Recreation.jsx
+++ src/pages/Recreation.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import styled from 'styled-components';
+import RecreationCard from '../components/Card/RecreationCard';
 
 const RecreationLayout = styled.div``;
 
-const Title = styled.h1``;
+const Title = styled.h1`
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 4rem;
+`;
 
-const RecreationList = styled.ul``;
-
-const RecreationItem = styled.li``;
+const RecreationList = styled.ul`
+  width: 100%;
+  display: grid;
+  gap: 1.5rem;
+  padding: 0 2rem;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
+`;
 
 const Recreation = () => {
   return (
     <RecreationLayout>
       <Title>레크레이션</Title>
       <RecreationList>
-        <RecreationItem>asd</RecreationItem>
+        {Array.from(Array(100), () => (
+          <RecreationCard />
+        ))}
       </RecreationList>
     </RecreationLayout>
   );
