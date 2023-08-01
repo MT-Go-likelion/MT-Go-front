7e531f3449a0e21f3c33e8e032e6d0ebba81cd9c
minjeoong <orange_mj@naver.com>
2023-08-01 14:40:29 +0900

    숙소예약페이지

---
+++ src/components/SelectBox/Date.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import COLOR from '../../constants/color';
+
+const BoxFlex = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const StyledDatePickerstart = styled(DatePicker)`
+  border-radius: 30px 0 0 30px;
+  background: #${COLOR.white};
+  width: 150px;
+  height: 33px;
+  padding: 6px;
+  color: ${COLOR.black};
+  padding-left: 14px;
+`;
+
+const StyledDatePickerend = styled(DatePicker)`
+  border-radius: 0 30px 30px 0;
+  background: #${COLOR.white};
+  width: 150px;
+  height: 33px;
+  padding: 6px;
+  color: ${COLOR.black};
+  padding-left: 14px;
+  &:: placeholder {
+	    color: {COLOR.lightGray};
+  }
+`;
+
+const DateRangePicker = () => {
+  const [departureDate, setDepartureDate] = useState(null);
+  const [arrivalDate, setArrivalDate] = useState(null);
+
+  return (
+    <BoxFlex>
+      <div>
+        <div>
+          <StyledDatePickerstart
+            id="departureDate"
+            selected={departureDate}
+            onChange={(date) => setDepartureDate(date)}
+            selectsStart
+            startDate={departureDate}
+            endDate={arrivalDate}
+            placeholderText="출발일"
+            placeholderTextColor={COLOR.lightGray}
+          />
+        </div>
+      </div>
+      <div>
+        <div>
+          <StyledDatePickerend
+            id="arrivalDate"
+            selected={arrivalDate}
+            onChange={(date) => setArrivalDate(date)}
+            selectsEnd
+            startDate={departureDate}
+            endDate={arrivalDate}
+            minDate={departureDate}
+            placeholderText="도착일"
+            placeholderTextColor={COLOR.lightGray}
+          />
+        </div>
+      </div>
+    </BoxFlex>
+  );
+};
+
+export default DateRangePicker;
--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,15 +1,102 @@
 import React from 'react';
+import styled from 'styled-components';
+import COLOR from '../constants/color';
+
+import Location from '../components/SelectBox/Location';
+import Headcount from '../components/SelectBox/Headcount';
+import Price from '../components/SelectBox/Price';
+import Date from '../components/SelectBox/Date';
+
 import Bestlocation from '../components/Card/Bestlocation';
-import Recreation from '../components/Card/Recreation';
-import Bag from '../components/Card/Bag';
+import SearchBackgroundIMG from '../assets/images/1_background.png';
+
+// 검색 바 백그라운드 이미지
+const SearchBack = styled.div`
+  width: 100%;
+  height: 330px;
+  flex-shrink: 0;
+  background-image: url(${SearchBackgroundIMG});
+  background-size: cover;
+  background-position: center;
+  background-blend-mode: multiply, normal;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 1.5rem;
+`;
+
+const ContentsDiv = styled.div`
+  padding: 5rem 10rem;
+  display: flex;
+  gap: 2rem;
+  width: 100%;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+// 검색바 타이틀
+const Title = styled.div`
+  color: ${COLOR.white};
+  text-align: center;
+  font-size: 30px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 140%;
+`;
+
+// 위치 인원 가격 flex
+const BoxFlex = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 3rem;
+`;
+
+// 검색하기 버튼
+const SearchBtn = styled.button`
+  border-radius: 32px;
+  border: 4px solid ${COLOR.white};
+  background: none;
+  width: 130px;
+  height: 45px;
+  color: ${COLOR.white};
+`;
+
+const PriceOPTION = [
+  { value: '', name: '가격' },
+  { value: '10', name: '10만원 이하' },
+  { value: '20', name: '20만원 이하' },
+  { value: '30', name: '30만원 이하' },
+];
 
 const Room = () => {
   return (
     <div>
-      <Bestlocation />
-      <Recreation />
-      <Bag />
+      <SearchBack>
+        <Title>원하는 단체숙소를 검색하세요!</Title>
+        <BoxFlex>
+          <Location options={PriceOPTION} />
+          <Headcount />
+          <Price />
+        </BoxFlex>
+        <BoxFlex>
+          <Date />
+        </BoxFlex>
+        <SearchBtn>검색하기</SearchBtn>
+      </SearchBack>
+      <ContentsDiv>
+        <Bestlocation />
+        <Bestlocation />
+        <Bestlocation />
+        <Bestlocation />
+        <Bestlocation />
+        <Bestlocation />
+        <Bestlocation />
+        <Bestlocation />
+      </ContentsDiv>
     </div>
   );
 };
+
 export default Room;
