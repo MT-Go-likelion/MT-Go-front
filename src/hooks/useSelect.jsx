922e1c58fb39e6425292a850f9e4bd183c0b702f
cychann <skcbdlhc129@naver.com>
2023-08-01 17:34:09 +0900

    fix: useSelect initialValue 수정

--- src/hooks/useSelect.jsx
+++ src/hooks/useSelect.jsx
@@ -1,6 +1,6 @@
 import { useState, useCallback } from 'react';
 
-const useSelect = (initialValue = []) => {
+const useSelect = (initialValue = '') => {
   const [value, setValue] = useState(initialValue);
   const handler = useCallback((e) => {
     setValue(e.target.value);
