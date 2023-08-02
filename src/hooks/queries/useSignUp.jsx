812a8805f027d995ce3a4785adaf7f93a549c04c
cychann <skcbdlhc129@naver.com>
2023-08-03 06:38:10 +0900

    api: 로그인한 user 정보 가져오기 & 로그인 상태 유지

--- src/hooks/queries/useSignUp.jsx
+++ src/hooks/queries/useSignUp.jsx
@@ -1,44 +1,28 @@
 import { useMutation } from '@tanstack/react-query';
 import axios from 'axios';
-// import { useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+async function signUp(payload) {
+  const response = axios
+    .post('http://110.11.183.148:8000/accounts/user/signup/', payload)
+    .then((res) => res.data);
+
+  return response;
+}
 
 const useSignUpMutation = () => {
-  //   const navigate = useNavigate();
-  const { mutate: signUpMutation } = useMutation(
-    (payLoad) => {
-      // signUp(payLoad)
-      console.log(payLoad);
-      // axios.post('http://110.11.183.148:8000/accounts/user/signup/')
-      axios.post('https://reqres.in/api/users', payLoad).then((res) => console.log(res));
+  const navigate = useNavigate();
+  const { mutate: signUpMutation } = useMutation((payload) => signUp(payload), {
+    onSuccess: () => {
+      navigate('/signin');
     },
-    {
-      onSuccess: () => {
-        //
-        // navigate('/signin');
-      },
-      onError: (error) => {
-        //
-        console.error(error);
-      },
+    onError: (error) => {
+      //
+      console.error(error);
     },
-  );
+  });
 
   return { signUpMutation };
 };
 
 export default useSignUpMutation;
-
-// axios
-//   .post('http://110.11.183.148:8000/accounts/user/signup/', {
-//     email,
-//     password,
-//     name,
-//   })
-//   .then((response) => {
-//     // 요청 성공 시 처리
-//     console.log(response.data);
-//   })
-//   .catch((error) => {
-//     // 요청 실패 시 처리
-//     console.error(error);
-//   });
