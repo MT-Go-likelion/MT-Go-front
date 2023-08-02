3020057a00f78772011d209cc9a2159cf6443296
minjeoong <orange_mj@naver.com>
2023-08-02 13:44:31 +0900

    feat: 배너 이미지 리스트 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -11,44 +11,47 @@ import RecreationCard from '../components/Card/RecreationCard';
 
 const MainLayout = styled.div`
   width: 100%;
-  height: 100%;
-  margin-bottom: 4rem;
+  height: 80%;
 `;
 
 // 배너 - 메인 img
 const Banner = styled.div`
   width: 100%;
-  position: relative; /* 추가: Banner를 상대적으로 배치 */
-  margin-bottom: 10px; /* 추가: Banner 아래로 10px 마진 설정 */
+  position: relative;
 `;
 
 const BannerImg = styled.img`
   width: 100%;
 `;
 
-// 배너 - 리스트 3개
-// const Tmpul = styled.ul`
-//   width: 100%;
-//   display: flex;
-//   flex-wrap: wrap;
-//   gap: 2rem;
-//   flex-direction: row;
-//   justify-content: center;
-//   position: absolute;
-//   top: 185px;
-// `;
+const Listdiv = styled.div`
+  position: relative;
+  top: -10rem;
+`;
 
-// const TmpLi = styled.li`
-//   width: 10rem;
-//   height: 12rem;
-//   background-color: ${COLOR.black};ss
-//   opacity: 0.5%;
-// `;
+// 배너 - 리스트 3개
+const Tmpul = styled.ul`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 2rem;
+  flex-direction: row;
+  justify-content: center;
+`;
+
+const TmpLi = styled.li`
+  width: 14%;
+  height: 15rem;
+  background-color: ${COLOR.black};
+  opacity: 0.5;
+`;
 
 // 전체 적용 마진
 const ContentLayout = styled.div`
   margin: 0 6rem;
   padding: 0 3rem;
+  top: -7rem;
+  position: relative;
 `;
 const Flexdiv = styled.div`
   display: flex;
@@ -94,18 +97,20 @@ const Main = () => {
         <Banner>
           <BannerImg src={MainBanner} />
         </Banner>
-        {/* <Tmpul>
-          <TmpLi>리스트1</TmpLi>
-          <TmpLi>리스트1</TmpLi>
-          <TmpLi>리스트1</TmpLi>
-        </Tmpul> */}
+        <Listdiv>
+          <Tmpul>
+            <TmpLi>리스트1</TmpLi>
+            <TmpLi>리스트1</TmpLi>
+            <TmpLi>리스트1</TmpLi>
+          </Tmpul>
+        </Listdiv>
       </MainLayout>
 
       <ContentLayout>
         <Title>추천 단체 숙소</Title>
         <Divstyled>
           <SubTitle>마음에 드는 숙박 장소를 골라보세요.</SubTitle>
-          <More>더보기</More>
+          <More href="#">더보기</More>
         </Divstyled>
         <Flexdiv>
           <BestlocationCard />
@@ -117,7 +122,7 @@ const Main = () => {
         <Title>장보기</Title>
         <Divstyled>
           <SubTitle>파트너들과 함께 장바구니에 담을 물품들을 골라보세요.</SubTitle>
-          <More>더보기</More>
+          <More href="#">더보기</More>
         </Divstyled>
         <Flexdiv>
           <BagCard />
@@ -129,7 +134,7 @@ const Main = () => {
         <Title>추천 레크레이션</Title>
         <Divstyled>
           <SubTitle>MT의 꽃, 레크레이션! 만약 무엇을 할지 모르겠다면?</SubTitle>
-          <More>더보기</More>
+          <More href="#">더보기</More>
         </Divstyled>
         <Flexdiv>
           <RecreationCard />
