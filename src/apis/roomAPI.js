0188841c6d3c2ff8afe58586502423a66d85cd40
cychann <skcbdlhc129@naver.com>
2023-08-06 15:50:17 +0900

    refactor: 숙소 list, scrap 관련 api 코드 분리

---
+++ src/apis/roomAPI.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { ROOMAPI } from '../config/api';
+
+const roomAPI = {
+  list: async () => {
+    const res = await axios.get(ROOMAPI.MAINLIST);
+    return res.data;
+  },
+  scrap: async ({ isScrap, lodging, token }) => {
+    const headers = token ? { headers: { Authorization: `Token ${token}` } } : { headers: {} };
+    const res = await axios.post(ROOMAPI.SCRAP, { isScrap, lodging }, headers);
+    return res.data;
+  },
+};
+
+export default roomAPI;
--- src/config/api.js
+++ src/config/api.js
@@ -1,15 +1,12 @@
-// import axios from 'axios';
-
-const BASE_URL = 'http://54.180.98.209:8000';
+export const BASE_URL = 'http://54.180.98.209:8000';
 
 export const API = {
   SIGNUP: `${BASE_URL}/accounts/user/signup/`,
   SIGNIN: `${BASE_URL}/accounts/user/signin/`,
 };
 
-export const RoomAPI = {
+export const ROOMAPI = {
   MAINLIST: `${BASE_URL}/lodging/main/`,
   SCRAP: `${BASE_URL}/lodging/scrap/`,
   CREATEROOM: `${BASE_URL}/lodging/create/`,
-  BASE_URL: `${BASE_URL}`,
 };
