b91c64eab56acc5fcf36e9b20149ac4198e59abf
cychann <skcbdlhc129@naver.com>
2023-08-11 20:52:38 +0900

    fix: 개인스페이스 장바구니 실시간 반영되도록

--- src/hooks/queries/Shopping/useShopping.jsx
+++ src/hooks/queries/Shopping/useShopping.jsx
@@ -2,9 +2,7 @@ import { useMutation, useQuery } from '@tanstack/react-query';
 import shoppingAPI from '../../../apis/shoppingAPI';
 
 const useShopping = (token) => {
-  const shoppingQuery = useQuery(['shopping'], () => shoppingAPI.list(token), {
-    enabled: !!token,
-  });
+  const shoppingQuery = useQuery(['shopping'], () => shoppingAPI.list(token));
 
   const { mutate: shoppingMutation } = useMutation(
     (payload) => shoppingAPI.create(payload, token),
