efacf5eba67fb4e6ba0f912740c4085b1bbb289a
minjeoong <orange_mj@naver.com>
2023-08-13 17:59:02 +0900

    feat: 레크레이션 디테일 페이지

--- src/pages/RecreationDetail.jsx
+++ src/pages/RecreationDetail.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
 
 import { useNavigate, useParams } from 'react-router-dom';
 import COLOR from '../constants/color';
@@ -8,6 +8,8 @@ import Loading from './Loading';
 import Error from './Error';
 import { BASE_URL } from '../config/api';
 import useRecreation from '../hooks/queries/Recreation/useRecreation';
+import Backimg from '../assets/images/chevron-left.png';
+import { mobileSize } from '../utils/MediaSize';
 
 const RecreationDetailLayout = styled.div`
   max-width: 1280px;
@@ -15,6 +17,10 @@ const RecreationDetailLayout = styled.div`
   margin-top: 4rem;
   display: flex;
   flex-direction: column;
+  @media (max-width: ${mobileSize}px) {
+    width: 100%;
+    margin-top: 0;
+  }
 `;
 
 const RecreationContentContainer = styled.div`
@@ -35,6 +41,10 @@ const RecreationImg = styled.img`
   width: 100%;
   height: 40rem;
   border-radius: 1rem;
+  @media (max-width: ${mobileSize}px) {
+    width: 100%;
+    height: 350px;
+  }
 `;
 const TeamspaceBtn = styled.button`
   padding: 0.2rem 0.8rem;
@@ -66,13 +76,91 @@ const RightContainerHeader = styled.div`
 const RecommendedNum = styled.div`
   font-size: 1.5rem;
   margin-bottom: 0.4rem;
+  @media (max-width: ${mobileSize}px) {
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 160%;
+  }
 `;
 
 const RecreationDetailContent = styled.div`
   line-height: 1.5;
 `;
 
+// Mobile
+const Header = styled.div`
+  height: 4.5rem;
+  background-color: ${COLOR.white};
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin: 0.5rem 2rem;
+  align-items: center;
+`;
+
+const Back = styled.img`
+  width: 32px;
+  height: 32px;
+`;
+
+const FlexDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 1rem;
+  align-items: center;
+`;
+
+const TeamButton = styled.button`
+  width: 32px;
+  height: 32px;
+  border-radius: 50px;
+  background-color: ${COLOR.primary.blue};
+`;
+
+const ContentDiv = styled.div`
+  width: 100%;
+  border-radius: 32px 32px 0px 0px;
+  background: ${COLOR.white};
+  padding: 3rem 2rem;
+  box-shadow: 0px -4px 8px 0px rgba(0, 0, 0, 0.1);
+  transition: transform 0.3s ease;
+  animation: ${(props) => (props.isLifted ? liftAnimation : 'none')} 0.4s ease;
+`;
+
+const ButtonDiv = styled.div`
+  padding: 0 2rem;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 32px;
+  transition: transform 0.3s ease;
+  animation: ${(props) => (props.isLifted ? liftAnimation : 'none')} 0.4s ease;
+`;
+const SubButton = styled.button`
+  width: 100%;
+  text-align: center;
+  border-radius: 12px 12px 0px 0px;
+  background: ${(props) => (props.isActive ? COLOR.white : COLOR.lightGray)};
+  box-shadow: 0px -4px 8px 0px rgba(0, 0, 0, 0.1);
+`;
+
+const liftAnimation = keyframes`
+  0% {
+    transform: translateY(0);
+  }
+  50% {
+    transform: translateY(-20px);
+  }
+  100% {
+    transform: translateY(0);
+  }
+`;
+
 const RecreationDetail = () => {
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= mobileSize);
+  const [activeButton, setActiveButton] = useState('rule');
+  const [isLifted, setIsLifted] = useState(false);
   const { recreationId } = useParams();
   const {
     lodgingDetailQuery: { isLoading, error, data: recreationDetail },
@@ -85,34 +173,108 @@ const RecreationDetail = () => {
     recreationDeleteMutation(recreationDetail.pk);
     navigate('/');
   };
+
+  const handleResize = () => {
+    setIsMobile(window.innerWidth <= mobileSize);
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <RecreationDetailLayout>
       {isLoading && <Loading />}
       {error && <Error />}
 
       {recreationDetail && (
-        <>
-          <RecreationTitle>{recreationDetail.name}</RecreationTitle>
-          <RecreationContentContainer>
-            <RecreationDetailLeftContiner>
+        <div>
+          {isMobile ? (
+            <>
+              <Header>
+                <Back src={Backimg} onClick={() => navigate('/Recreation')} />
+                <FlexDiv>
+                  <TeamButton>담기</TeamButton>
+                  <TeamButton onClick={onClickDeleteBtn}>삭제</TeamButton>
+                </FlexDiv>
+              </Header>
               <RecreationImg src={BASE_URL + recreationDetail.photo} />
-            </RecreationDetailLeftContiner>
-            <RecreationDetailRightContainer>
-              <RightContainerHeader>
-                <RecommendedNum>
-                  추천인원: {recreationDetail.headCountMin} ~ {recreationDetail.headCountMax}명
-                </RecommendedNum>
-                <TeamspaceBtn>팀스페이스 담기</TeamspaceBtn>
-                <TeamspaceBtn onClick={onClickDeleteBtn}>삭제하기</TeamspaceBtn>
-              </RightContainerHeader>
-              <RecreationDetailContent
-                dangerouslySetInnerHTML={{ __html: recreationDetail.content }}
-              >
-                {}
-              </RecreationDetailContent>
-            </RecreationDetailRightContainer>
-          </RecreationContentContainer>
-        </>
+              <ButtonDiv isLifted={isLifted}>
+                <SubButton
+                  onClick={() => {
+                    setActiveButton('rule');
+                    setIsLifted(true);
+
+                    // After a short delay, reset the lifted state
+                    setTimeout(() => {
+                      setIsLifted(false);
+                    }, 300); // Adjust the delay as needed
+                  }}
+                  isActive={activeButton === 'rule'}
+                >
+                  규칙
+                </SubButton>
+                <SubButton
+                  onClick={() => {
+                    setActiveButton('keyWord');
+                    setIsLifted(true);
+
+                    // After a short delay, reset the lifted state
+                    setTimeout(() => {
+                      setIsLifted(false);
+                    }, 300); // Adjust the delay as needed
+                  }}
+                  isActive={activeButton === 'keyWord'}
+                >
+                  키워드
+                </SubButton>
+              </ButtonDiv>
+              <ContentDiv isLifted={isLifted}>
+                {activeButton === 'rule' && (
+                  <div>
+                    {recreationDetail.name}{' '}
+                    <RecommendedNum>
+                      추천인원: {recreationDetail.headCountMin} ~ {recreationDetail.headCountMax}명
+                    </RecommendedNum>
+                    <RecreationDetailContent
+                      dangerouslySetInnerHTML={{ __html: recreationDetail.content }}
+                    >
+                      {}
+                    </RecreationDetailContent>
+                  </div>
+                )}
+                {activeButton === 'keyWord' && <div>키워드 내용</div>}
+              </ContentDiv>
+            </>
+          ) : (
+            <>
+              <RecreationTitle>{recreationDetail.name}</RecreationTitle>
+              <RecreationContentContainer>
+                <RecreationDetailLeftContiner>
+                  <RecreationImg src={BASE_URL + recreationDetail.photo} />
+                </RecreationDetailLeftContiner>
+                <RecreationDetailRightContainer>
+                  <RightContainerHeader>
+                    <RecommendedNum>
+                      추천인원: {recreationDetail.headCountMin} ~ {recreationDetail.headCountMax}명
+                    </RecommendedNum>
+                    <TeamspaceBtn>팀스페이스 담기</TeamspaceBtn>
+                    <TeamspaceBtn onClick={onClickDeleteBtn}>삭제하기</TeamspaceBtn>
+                  </RightContainerHeader>
+                  <RecreationDetailContent
+                    dangerouslySetInnerHTML={{ __html: recreationDetail.content }}
+                  >
+                    {}
+                  </RecreationDetailContent>
+                </RecreationDetailRightContainer>
+              </RecreationContentContainer>
+            </>
+          )}
+        </div>
       )}
     </RecreationDetailLayout>
   );
