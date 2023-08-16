6ff0451384b1f1d6599563d7f39c0b80e88f6ce3
cychann <skcbdlhc129@naver.com>
2023-08-16 16:42:36 +0900

    fix: 레크레이션 수정 페이지 수정

--- src/pages/UpdateLodging.jsx
+++ src/pages/UpdateLodging.jsx
@@ -7,6 +7,7 @@ import useInput from '../hooks/useInput';
 import useLodgingUpdate from '../hooks/queries/Lodging/useLodgingUpdate';
 import HorizonLine from '../components/Common/Line/HorizonLine';
 import { BASE_URL } from '../config/api';
+import ApiCallSuccessPopup from '../components/Common/Popup/ApiCallSuccessPopup';
 
 const FormContainer = styled.div`
   display: flex;
@@ -35,11 +36,6 @@ const SubmitButton = styled.button`
   cursor: pointer;
 `;
 
-const SuccessText = styled.div`
-  font-size: 1.75rem;
-  font-size: bold;
-`;
-
 const ThumbImgContainer = styled.div`
   width: 10rem;
   height: 10rem;
@@ -57,16 +53,27 @@ const ImgText = styled.div`
 
 const ThumbDelteBtn = styled.div``;
 
+const InputContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin: 1rem 0;
+
+  margin-bottom: ${(props) => props.mb && `${props.mb}rem`};
+`;
+
+const FormLabel = styled.label`
+  flex-basis: ${(props) => (props.fb ? `${props.fb}%` : '5%')};
+`;
+
 const UpdateLodging = () => {
   const queryClient = useQueryClient();
   const user = queryClient.getQueryData(['user']);
-
   const location = useLocation();
-
   const [name, onChangeName] = useInput(location.state.lodging.name);
   const [address, onChangeAddress] = useInput(location.state.lodging.address);
   const [place, onChangePlace] = useInput(location.state.lodging.place);
-  const [price, onChangePrice] = useInput(location.state.lodging.price);
   const [phoneNumber, onChangePhoneNumber] = useInput(location.state.lodging.phoneNumber);
   const [amenities, onChangeAmenities] = useInput(location.state.lodging.amenities);
   const [homePageURL, onChangeHomePageURL] = useInput(location.state.lodging.homePageURL);
@@ -75,6 +82,18 @@ const UpdateLodging = () => {
   const [precaution, onChangePrecaution] = useInput(location.state.lodging.precaution);
   const [checkInTime, onChangeCheckInTime] = useInput(location.state.lodging.checkInTime);
   const [checkOutTime, onChangeCheckOutTime] = useInput(location.state.lodging.checkOutTime);
+  const [peakWeekendPrice, onChangePeakWeekendPrice] = useInput(
+    location.state.lodging.peakWeekendPrice,
+  );
+  const [peakWeekdayPrice, onChangePeakWeekdayPrice] = useInput(
+    location.state.lodging.peakWeekdayPrice,
+  );
+  const [lowWeekendPrice, onChangeLowWeekendPrice] = useInput(
+    location.state.lodging.lowWeekendPrice,
+  );
+  const [lowWeekdayPrice, onChangeLowWeekdayPrice] = useInput(
+    location.state.lodging.lowWeekdayPrice,
+  );
   const [myImage, setMyImage] = useState([]);
   const [mainPhoto, setMainPhoto] = useState(location.state.lodging.mainPhoto);
   const [addedMainPhoto, setAddedMainPhoto] = useState('');
@@ -131,7 +150,6 @@ const UpdateLodging = () => {
     formData.append('name', name);
     formData.append('address', address);
     formData.append('place', place);
-    formData.append('price', price);
     formData.append('phoneNumber', phoneNumber);
     formData.append('amenities', amenities);
     formData.append('homePageURL', homePageURL);
@@ -140,6 +158,10 @@ const UpdateLodging = () => {
     formData.append('precaution', precaution);
     formData.append('checkInTime', checkInTime);
     formData.append('checkOutTime', checkOutTime);
+    formData.append('peakWeekendPrice', peakWeekendPrice);
+    formData.append('peakWeekdayPrice', peakWeekdayPrice);
+    formData.append('lowWeekendPrice', lowWeekendPrice);
+    formData.append('lowWeekdayPrice', lowWeekdayPrice);
 
     if (mainPhoto !== location.state.lodging.mainPhoto) {
       formData.append('mainPhoto', addedMainPhoto);
@@ -153,8 +175,7 @@ const UpdateLodging = () => {
     });
     if (user) {
       lodgingUpdateMutation(formData, {
-        onSuccess: (data) => {
-          console.log(data);
+        onSuccess: () => {
           setSuccess('✅ 숙소가 성공적으로 업데이트 되었습니다!');
           setTimeout(() => setSuccess(null), 3000);
         },
@@ -165,11 +186,10 @@ const UpdateLodging = () => {
   };
   return (
     <FormContainer>
-      {success && <SuccessText className="text-2xl font-semibold">{success}</SuccessText>}
+      <ApiCallSuccessPopup success={success} />
       <form onSubmit={handlesubmit} method="POST">
         <FormInput type="text" placeholder="숙소이름" value={name} onChange={onChangeName} />
         <FormInput type="text" placeholder="address" value={address} onChange={onChangeAddress} />
-        <FormInput type="text" placeholder="price" value={price} onChange={onChangePrice} />
         <FormInput type="text" placeholder="place" value={place} onChange={onChangePlace} />
         <FormInput
           type="text"
@@ -214,6 +234,44 @@ const UpdateLodging = () => {
           value={checkOutTime}
           onChange={onChangeCheckOutTime}
         />
+
+        <InputContainer>
+          <FormLabel fb={20}>가격(성수기 주말 및 공휴일):</FormLabel>
+          <FormInput
+            fb={80}
+            type="text"
+            value={peakWeekendPrice}
+            onChange={onChangePeakWeekendPrice}
+          />
+        </InputContainer>
+        <InputContainer>
+          <FormLabel fb={20}>가격(성수기 평일):</FormLabel>
+          <FormInput
+            fb={80}
+            type="text"
+            value={peakWeekdayPrice}
+            onChange={onChangePeakWeekdayPrice}
+          />
+        </InputContainer>
+        <InputContainer>
+          <FormLabel fb={20}>가격(비성수기 주말 및 공휴일):</FormLabel>
+          <FormInput
+            fb={80}
+            type="text"
+            value={lowWeekendPrice}
+            onChange={onChangeLowWeekendPrice}
+          />
+        </InputContainer>
+        <InputContainer>
+          <FormLabel fb={20}>가격(비성수기 평일):</FormLabel>
+          <FormInput
+            fb={80}
+            type="text"
+            value={lowWeekdayPrice}
+            onChange={onChangeLowWeekdayPrice}
+          />
+        </InputContainer>
+
         <input type="file" onChange={handleImageChange} />
         <ImgText>메인사진</ImgText>
 
