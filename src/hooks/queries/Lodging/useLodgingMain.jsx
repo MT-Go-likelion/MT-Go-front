fec9c710431036fb50b6fdfc994d9c2f87d21c0b
cychann <skcbdlhc129@naver.com>
2023-08-18 03:28:57 +0900

    feat: 토큰 만료 시 로그인 페이지로

--- src/hooks/queries/Lodging/useLodgingMain.jsx
+++ src/hooks/queries/Lodging/useLodgingMain.jsx
@@ -1,8 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 import lodgingAPI from '../../../apis/lodgingAPI';
+import { useSignOut } from '../Auth/useSignOut';
 
 const useLodgingMain = (token) => {
-  const mainLodgingsQuery = useQuery(['lodgings', token], () => lodgingAPI.mainList(token));
+  const signOut = useSignOut();
+  const navigate = useNavigate();
+
+  const mainLodgingsQuery = useQuery(['lodgings', token], () => lodgingAPI.mainList(token), {
+    onError: (error) => {
+      if (error.response.data.detail === '토큰이 유효하지 않습니다.') {
+        signOut();
+        navigate('/signin');
+      }
+    },
+  });
 
   return { mainLodgingsQuery };
 };
