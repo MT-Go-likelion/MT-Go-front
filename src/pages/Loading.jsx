2248014640c137f62a902155ea350f4774515982
cychann <skcbdlhc129@naver.com>
2023-08-18 03:29:05 +0900

    feat: 로딩 스피너 적용

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { ColorRing } from 'react-loader-spinner';
+import COLOR from '../constants/color';
 
 const Loading = () => {
-  return <div>로딩 중...</div>;
+  return (
+    <ColorRing
+      visible
+      height="80"
+      width="80"
+      ariaLabel="blocks-loading"
+      wrapperStyle={{}}
+      wrapperClass="blocks-wrapper"
+      colors={[COLOR.white, COLOR.primary.lightBlue, COLOR.primary.blue, '#abbd81', '#849b87']}
+    />
+  );
 };
 
 export default Loading;
