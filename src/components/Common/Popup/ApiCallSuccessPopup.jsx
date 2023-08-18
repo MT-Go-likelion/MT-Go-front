1ad860ba2c610ff0ef50e0a05758c10f7e2bff06
cychann <skcbdlhc129@naver.com>
2023-08-18 18:10:32 +0900

    fix: 모바일 api 성공 팝업창 크기 조절

--- src/components/Common/Popup/ApiCallSuccessPopup.jsx
+++ src/components/Common/Popup/ApiCallSuccessPopup.jsx
@@ -4,17 +4,19 @@ import COLOR from '../../../constants/color';
 const Popup = styled.div`
   position: fixed;
   bottom: 0;
-  left: 40%;
+  left: ${(props) => (props.isMobile ? '15%' : '40%')};
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 23rem;
+  width: ${(props) => (props.isMobile ? '18rem' : '23rem')};
   height: 3rem;
   background-color: ${COLOR.primary.blue};
   color: ${COLOR.white};
   padding: 0.5rem 0;
   border-radius: 1rem;
   font-size: 1rem;
+  font-size: ${(props) => (props.isMobile ? '0.8rem' : '1rem')};
+
   z-index: 9999;
   opacity: ${(props) => (props.$success ? 1 : 0)};
   transform: ${(props) => (props.$success ? 'translateY(-3rem)' : 'none')};
@@ -22,7 +24,11 @@ const Popup = styled.div`
 `;
 
 const ApiCallSuccessPopup = ({ success }) => {
-  return <Popup $success={success}>{success}</Popup>;
+  return (
+    <Popup $success={success} isMobile={window.innerWidth < 600}>
+      {success}
+    </Popup>
+  );
 };
 
 export default ApiCallSuccessPopup;
